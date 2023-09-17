64a69f16a1c071db812f20db321305f670c82cf2
miltone92 <55605034+miltone92@users.noreply.github.com>
2023-09-17 13:37:09 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: '/vecttor',
+  assetPrefix: '/vecttor/',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
